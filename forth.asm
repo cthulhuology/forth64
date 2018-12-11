c5e7aa0c98885873f4d0d4e0c96f50fcd4280bc8
Dave Goehrig <dave@dloh.org>
2018-12-11 08:48:47 +0000

    Adding immediate flag to immediates

--- forth.asm
+++ forth.asm
@@ -1,6 +1,9 @@
 
 BITS 64
 
+;; macros to make life easier
+%define imm(x) (x|0x80)
+
 ;;; This is the lexicon, we'll put this somewhere eventually
 lexicon:	dq lexicon
 		db 7,'lexicon'
@@ -13,61 +16,61 @@ pad:		dq 0xfeedface
 source:		dq 0xc0de
 		db 6,'source',0
 plus:		dq add_op
-		db 1,'+',0,0,0,0,0,0
+		db imm(1),'+',0,0,0,0,0,0
 minus:		dq sub_op
-		db 1,'-',0,0,0,0,0,0
+		db imm(1),'-',0,0,0,0,0,0
 star:		dq mul_op
-		db 1,'*',0,0,0,0,0,0
+		db imm(1),'*',0,0,0,0,0,0
 slash:		dq div_op
-		db 1,'/',0,0,0,0,0,0
+		db imm(1),'/',0,0,0,0,0,0
 percent:	dq mod_op
-		db 1,'%',0,0,0,0,0,0
+		db imm(1),'%',0,0,0,0,0,0
 negate:		dq neg_op
-		db 1,'~',0,0,0,0,0,0
+		db imm(1),'~',0,0,0,0,0,0
 amp:		dq and_op
-		db 1,'&',0,0,0,0,0,0
+		db imm(1),'&',0,0,0,0,0,0
 pipe:		dq or_op
-		db 1,'|',0,0,0,0,0,0
+		db imm(1),'|',0,0,0,0,0,0
 carrot:		dq xor_op
-		db 1,'^',0,0,0,0,0,0
+		db imm(1),'^',0,0,0,0,0,0
 bang:		dq not_op
-		db 1,'!',0,0,0,0,0,0
+		db imm(1),'!',0,0,0,0,0,0
 equal:		dq equ_op
-		db 1,'=',0,0,0,0,0,0
+		db imm(1),'=',0,0,0,0,0,0
 lesser:		dq lesser_op
-		db 1,'<',0,0,0,0,0,0
+		db imm(1),'<',0,0,0,0,0,0
 greater:	dq greater_op
-		db 1,'>',0,0,0,0,0,0
+		db imm(1),'>',0,0,0,0,0,0
 branch:		dq branch_op
-		db 1,'?',0,0,0,0,0,0
+		db imm(1),'?',0,0,0,0,0,0
 fetch:		dq fetch_op
-		db 1,'@',0,0,0,0,0,0
+		db imm(1),'@',0,0,0,0,0,0
 fetch_plus:	dq fetch_plus_op
-		db 2,'@+',0,0,0,0,0
+		db imm(2),'@+',0,0,0,0,0
 store:		dq store_op
-		db 1,'$',0,0,0,0,0,0
+		db imm(1),'$',0,0,0,0,0,0
 store_plus:	dq store_plus_op
-		db 1,'$+',0,0,0,0,0
+		db imm(2),'$+',0,0,0,0,0
 comma:		dq comma_op
-		db 1,',',0,0,0,0,0,0
+		db imm(1),',',0,0,0,0,0,0
 return:		dq ret_op
-		db 1,';',0,0,0,0,0,0
+		db imm(1),';',0,0,0,0,0,0
 call_word:	dq call_op
-		db 1,'.',0,0,0,0,0,0
+		db imm(1),'.',0,0,0,0,0,0
 lit:		dq lit_op
-		db 1,'#',0,0,0,0,0,0
+		db imm(1),'#',0,0,0,0,0,0
 quote:		dq quote_op
-		db 1,'`',0,0,0,0,0,0
+		db imm(1),'`',0,0,0,0,0,0
 tick:		dq tick_op
-		db 1,39,0,0,0,0,0,0
+		db imm(1),39,0,0,0,0,0,0
 string:		dq string_op
-		db 1,'\"',0,0,0,0,0,0
+		db imm(1),'"',0,0,0,0,0,0
 comments:	dq comment_op
-		db 1,'(',0,0,0,0,0,0
+		db imm(1),'(',0,0,0,0,0,0
 comm_end:	dq comment_end_op
-		db 1,')',0,0,0,0,0,0
+		db imm(1),')',0,0,0,0,0,0
 immediate:	dq immediate_op
-		db 1,'[',0,0,0,0,0,0
+		db imm(1),'[',0,0,0,0,0,0
 
 
 ;; This is the code section, need to put this somewhere too
